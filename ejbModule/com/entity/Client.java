02f29b323a03c72b9a5ec1082ecba3f16052477c
ness <ness@ness-X555LDB>
2018-12-22 12:36:02 +0100

    entity ajouter

---
+++ ejbModule/com/entity/Client.java
@@ -0,0 +1,46 @@
+package com.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client implements Serializable{
+	
+	@Id	
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	private String nom;
+	private String prenom;
+	
+	public Client() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	
+}
