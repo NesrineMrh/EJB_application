2d213f2332a77323b81cc588039c4249cbef81b7
inteex13 <inteex13@gmail.com>
2019-01-11 15:53:06 +0100

    pattern factory

--- ejbModule/com/entity/CompteProfessionnel.java
+++ ejbModule/com/entity/CompteProfessionnel.java
@@ -4,5 +4,16 @@ import javax.persistence.Entity;
 
 @Entity
 public class CompteProfessionnel extends Compte{
+	
+	private String adress ;
 
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	
+	
 }
