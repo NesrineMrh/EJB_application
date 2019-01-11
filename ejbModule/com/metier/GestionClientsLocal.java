772144b8deaf59471217b8e25b2485cca04e6cdb
inteex13 <inteex13@gmail.com>
2019-01-11 22:57:19 +0100

    conflit

--- ejbModule/com/metier/GestionClientsLocal.java
+++ ejbModule/com/metier/GestionClientsLocal.java
@@ -5,12 +5,16 @@ import java.util.List;
 import javax.ejb.Local;
 
 import com.entity.Client;
+import com.entity.ClientProfessionnel;
 
 @Local
 public interface GestionClientsLocal {
 	public Client ajouterClient(Client p);
+	public ClientProfessionnel ajouterClientProfessionnel(ClientProfessionnel c);
 	public boolean supprimerClient(int id);
 	public boolean modifierClient(Client p);
 	public Client afficherClient(int id);
 	public List<Client> listClient();
+	public List<Client> listClientByCompte(int code);
+
 }
--- ejbModule/com/metier/GestionClientsRemote.java
+++ ejbModule/com/metier/GestionClientsRemote.java
@@ -5,12 +5,15 @@ import java.util.List;
 import javax.ejb.Remote;
 
 import com.entity.Client;
+import com.entity.ClientProfessionnel;
 
 @Remote
 public interface GestionClientsRemote {
 	public Client ajouterClient(Client p);
+	public ClientProfessionnel ajouterClientProfessionnel(ClientProfessionnel c) ;
 	public boolean supprimerClient(int id);
 	public boolean modifierClient(Client p);
 	public Client afficherClient(int id);
 	public List<Client> listClient();
+	public List<Client> listClientByCompte(int code);
 }
